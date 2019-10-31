56ed0291b16086765ab7903d0532a673e0a417c5
Peter Nylander <peter.nylander@stratiteq.com>
2019-10-31 11:11:56 +0100

    Added sample app

---
+++ samples/AADIdentityClientSample/App.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace AADIdentityClientSample
+{
+    public class App
+    {
+        private readonly IService1WebApi service1WebApi;
+
+        public App(IService1WebApi service1WebApi)
+        {
+            this.service1WebApi = service1WebApi ?? throw new ArgumentNullException(nameof(service1WebApi));
+        }
+
+        public async Task RunAsync()
+        {
+            await this.service1WebApi.ExecuteAsync();
+        }
+    }
+}
---
+++ samples/AADIdentityClientSample/Startup.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Stratiteq.Identity.Client.AAD;
+using Stratiteq.IdentityClient;
+using System.IO;
+
+namespace AADIdentityClientSample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure =>
+                configure
+                    .AddDebug()
+                    .AddConsole());
+
+            // Build config
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            services.AddSingleton<IConfiguration>(configuration);
+
+            // Add services
+            services.AddTransient<App>();
+
+            services.AddSingleton<Service1IdentityClient>(serviceProvider =>
+                new Service1IdentityClient(
+                    new AADAppConfiguration(configuration.GetSection("Service1")),
+                    serviceProvider.GetService<ILogger<Service1IdentityClient>>()));
+
+            // These need to be transient so that a new instance is provided each time a new HttpClient is created.
+            services.AddTransient<AuthenticatingDelegatingHandler<Service1IdentityClient>>();
+
+            services.AddHttpClient<Service1ApiHttpClient>(client =>
+            {
+                client.BaseAddress = new System.Uri("https://localhost:44303/");
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            }).AddHttpMessageHandler<AuthenticatingDelegatingHandler<Service1IdentityClient>>();
+
+            services.AddTransient<IService1WebApi, Service1WebApi>();
+        }
+
+        public void Configure()
+        {
+        }
+    }
+}
